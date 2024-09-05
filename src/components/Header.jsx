8b054cb317cbdc44d200102835fe4a9582a29853
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-09-05 14:47:13 -0300

    [UPDATE] header padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { IoLogoWechat } from "react-icons/io5";
 const Header = () => {
   return (
     <section > 
-      <div class="flex flex-row justify-between items-center p-5 text-white"> 
+      <div class="flex flex-row justify-between items-center p-5 text-white lg:px-[200px]"> 
         <IoMenuOutline class="text-[40px] md:hidden"/>
         <h1 class="text-[20px]">DialogAI</h1>
         <IoLogoWechat class="text-[35px]"/> 
